b28039ad67de27519b53568e650599bb37a51a27
CodenameMaggie <maggie@maggieforbesstrategies.com>
2026-01-04 18:30:43 -0800

    Force styled-jsx regeneration by adding comment

--- app/page.tsx
+++ app/page.tsx
@@ -34,6 +34,7 @@ export default function LandingPage() {
   return (
     <>
       <style jsx global>{`
+        /* Classic Romance Palette v2 */
         @import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,500;0,600;1,400&family=Nunito:wght@300;400;500;600&display=swap');
 
         body {
